94a842f4d66a38ba3a9b14974a00e7326c135c1c
pickman22 <jhestolano@gmail.com>
2023-04-30 12:39:12 -0700

    Enable simple assertion for debugging.

--- sys/src/assert.c
+++ sys/src/assert.c
@@ -1,6 +1,21 @@
-#include "assert.h"
+typedef struct {
+  const char* file;
+  int line;
+  const char* func;
+  const char* failed_expr;
+} assert_info_t;
 
-inline void assert(bool expr)
+volatile assert_info_t g_assert_info = {0};
+
+void __assert_func(const char *file,
+                   int line,
+                   const char *func,
+                   const char *failedexpr)
 {
-
+  g_assert_info.file = file;
+  g_assert_info.line = line;
+  g_assert_info.func = func;
+  g_assert_info.failed_expr = failedexpr;
+  __asm("bkpt 1");
+  while(1);
 }
