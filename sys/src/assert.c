aaea7a55224fec1492caf0646d8b9b3be8d2e1dd
pickman22 <jhestolano@gmail.com>
2023-05-30 20:48:24 -0700

    Use DBG output in assert function.

--- sys/src/assert.c
+++ sys/src/assert.c
@@ -1,3 +1,5 @@
+#include "dbg.h"
+
 typedef struct {
   const char* file;
   int line;
@@ -12,10 +14,13 @@ void __assert_func(const char *file,
                    const char *func,
                    const char *failedexpr)
 {
-  g_assert_info.file = file;
-  g_assert_info.line = line;
-  g_assert_info.func = func;
-  g_assert_info.failed_expr = failedexpr;
+  DBG_ERR("%s:%s - %s: %s\n\r", file, line, func, failedexpr);
+  g_assert_info = (assert_info_t) {
+    .file = file,
+    .line = line,
+    .func = func,
+    .failed_expr = failedexpr,
+  };
   __asm("bkpt 1");
   while(1);
 }
